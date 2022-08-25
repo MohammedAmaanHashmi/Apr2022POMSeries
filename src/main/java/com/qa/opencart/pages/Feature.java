85ab846b8a13f386858d7fc75058e1117ed6a03a
MohammedAmaanHashmi <amaanhashmi98@gmail.com>
2022-08-25 23:11:39 +0530

    v3.0

--- src/main/java/com/qa/opencart/pages/Feature.java
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -1,18 +1,21 @@
 package com.qa.opencart.pages;
 
 public class Feature {
-	
-	public void f1()
-	{
+
+	public void f1() {
 		System.out.println("f1 feature");
 	}
-	public void f2()
-	{
+
+	public void f2() {
 		System.out.println("f2 feature");
 	}
 
-	public static void main(String[] args) {
-		
+	public void f3() {
+		System.out.println("f3 feature");
 	}
-	
+
+	public static void main(String[] args) {
+
+	}
+
 }
